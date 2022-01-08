08ca40f990842ab582eb2c649a93f47e6b6d6025
Olivier Sirol <czo@free.fr>
2022-01-08 13:25:57 +0100

    806 update

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -62,10 +62,10 @@ rm -rf /usr/share/man/man3
 
 # Cleanup XFCE menu
 sed -i '2 i NoDisplay=true' /usr/share/applications/{xfce4-mail-reader,xfce4-web-browser}.desktop
-sed -i "s/^\(Categories=\).*\$/Categories=Utility;/" /usr/share/applications/{geany,org.xfce.ristretto,*GHex*}.desktop
+sed -i "s/^\(Categories=\).*\$/Categories=Utility;/" /usr/share/applications/{geany,*ristretto*,*GHex*}.desktop
 
 # Remove large/irrelevant firmwares
-rm -rf /usr/lib/firmware/{liquidio,netronome,mellanox,mrvl/prestera}
+rm -rf /usr/lib/firmware/{liquidio,netronome,mellanox,mrvl/prestera,qcom}
 
 # Remove extra locales
 if [ -x /usr/bin/localepurge ]
